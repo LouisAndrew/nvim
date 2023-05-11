831c654834ff92de9bccde463f7e8d40aade702b
Louis <louisandrew3@gmail.com>
2023-05-11 11:50:24 +0200

    feat(nvim): add mappings and plugin support (MOPS-0)

---
+++ plugins.lua
@@ -0,0 +1,17 @@
+return require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+
+  use {
+    'ggandor/leap.nvim',
+    config = function()
+      require("leap").add_default_mappings()
+    end
+  }
+
+  use {
+    'numToStr/Comment.nvim',
+    config = function()
+      require('Comment').setup()
+    end
+  }
+end)
