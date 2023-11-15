fd79558a326ea509782179a188364b421819f97a
Louis <louis.andrew@free2move.com>
2023-11-15 16:32:40 +0100

    fix(lsp): setup lsp further

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -2,53 +2,53 @@ local lsp_zero = require("lsp-zero")
 local ft = require("guard.filetype")
 
 lsp_zero.on_attach(function(_, bufnr)
-  local opts = { buffer = bufnr, remap = false }
+	local opts = { buffer = bufnr, remap = false }
 
-  vim.keymap.set("n", "gd", function()
-    vim.lsp.buf.definition()
-  end, opts)
-  vim.keymap.set("n", "<leader>i", function()
-    vim.lsp.buf.hover()
-  end, opts)
-  vim.keymap.set("n", "<leader>vws", function()
-    vim.lsp.buf.workspace_symbol()
-  end, opts)
-  vim.keymap.set("n", "<leader>vd", function()
-    vim.diagnostic.open_float()
-  end, opts)
-  vim.keymap.set("n", "[d", function()
-    vim.diagnostic.goto_next()
-  end, opts)
-  vim.keymap.set("n", "]d", function()
-    vim.diagnostic.goto_prev()
-  end, opts)
-  vim.keymap.set("n", "<leader>vca", function()
-    vim.lsp.buf.code_action()
-  end, opts)
-  vim.keymap.set("n", "<leader>vrr", function()
-    vim.lsp.buf.references()
-  end, opts)
-  vim.keymap.set("n", "<leader>vrn", function()
-    vim.lsp.buf.rename()
-  end, opts)
-  vim.keymap.set("i", "<C-h>", function()
-    vim.lsp.buf.signature_help()
-  end, opts)
-  vim.keymap.set({ "n", "i" }, "<C-s>", function()
-    vim.lsp.buf.format({ async = true })
-  end, opts)
+	vim.keymap.set("n", "gd", function()
+		vim.lsp.buf.definition()
+	end, opts)
+	vim.keymap.set("n", "<leader>i", function()
+		vim.lsp.buf.hover()
+	end, opts)
+	vim.keymap.set("n", "<leader>vws", function()
+		vim.lsp.buf.workspace_symbol()
+	end, opts)
+	vim.keymap.set("n", "<leader>vd", function()
+		vim.diagnostic.open_float()
+	end, opts)
+	vim.keymap.set("n", "[d", function()
+		vim.diagnostic.goto_next()
+	end, opts)
+	vim.keymap.set("n", "]d", function()
+		vim.diagnostic.goto_prev()
+	end, opts)
+	vim.keymap.set("n", "<leader>vca", function()
+		vim.lsp.buf.code_action()
+	end, opts)
+	vim.keymap.set("n", "<leader>vrr", function()
+		vim.lsp.buf.references()
+	end, opts)
+	vim.keymap.set("n", "<leader>vrn", function()
+		vim.lsp.buf.rename()
+	end, opts)
+	vim.keymap.set("i", "<C-h>", function()
+		vim.lsp.buf.signature_help()
+	end, opts)
+	vim.keymap.set({ "n", "i" }, "<C-s>", function()
+		vim.lsp.buf.format({ async = true })
+	end, opts)
 end)
 
 require("mason").setup({})
 require("mason-lspconfig").setup({
-  ensure_installed = { "tsserver", "rust_analyzer", "eslint", "volar", "tailwindcss", "cssls" },
-  handlers = {
-    lsp_zero.default_setup,
-    lua_ls = function()
-      local lua_opts = lsp_zero.nvim_lua_ls()
-      require("lspconfig").lua_ls.setup(lua_opts)
-    end,
-  },
+	ensure_installed = { "tsserver", "rust_analyzer", "eslint", "volar", "tailwindcss", "cssls" },
+	handlers = {
+		lsp_zero.default_setup,
+		lua_ls = function()
+			local lua_opts = lsp_zero.nvim_lua_ls()
+			require("lspconfig").lua_ls.setup(lua_opts)
+		end,
+	},
 })
 
 --[[ lsp_zero.format_on_save({
@@ -66,18 +66,17 @@ local cmp = require("cmp")
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
 cmp.setup({
-  sources = {
-    { name = "path" },
-    { name = "nvim_lsp" },
-    { name = "nvim_lua" },
-  },
-  formatting = lsp_zero.cmp_format(),
-  mapping = cmp.mapping.preset.insert({
-    ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-    ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-    ["<C-y>"] = cmp.mapping.confirm({ select = true }),
-    ["<C-Space>"] = cmp.mapping.complete(),
-  }),
+	sources = {
+		{ name = "nvim_lsp" },
+		{ name = "nvim_lua" },
+	},
+	formatting = lsp_zero.cmp_format(),
+	mapping = cmp.mapping.preset.insert({
+		["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+		["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+		["<C-y>"] = cmp.mapping.confirm({ select = true }),
+		["<C-e>"] = cmp.mapping.complete(),
+	}),
 })
 
 ft("typescript,javascript,typescriptreact"):fmt("eslint_d")
@@ -85,8 +84,8 @@ ft("vue"):fmt("eslint_d")
 ft("lua"):fmt("lsp"):append("stylua")
 
 require("guard").setup({
-  -- the only options for the setup function
-  fmt_on_save = true,
-  -- Use lsp if no formatter was defined for this filetype
-  lsp_as_default_formatter = false,
+	-- the only options for the setup function
+	fmt_on_save = true,
+	-- Use lsp if no formatter was defined for this filetype
+	lsp_as_default_formatter = false,
 })
